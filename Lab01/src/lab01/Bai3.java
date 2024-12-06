c0cb12e10fde49c1d079444230b91e6daa170b04
NguyenMinh1301 <nguyenminhjqka@gmail.com>
2024-12-06 11:56:33 +0700

    NguyenMinh

--- Lab01/src/lab01/Bai3.java
+++ Lab01/src/lab01/Bai3.java
@@ -1,16 +1,18 @@
 package lab01;
-
+//Khai báo thư viện dùng Scanner
 import java.util.Scanner;
 
 public class Bai3 {
         public static void main(String[] args) {
+        //Tạo đối tượng scanner và các biến
         Scanner scanner = new Scanner(System.in);
         
+        //Mời người dùng nhập cạnh
         System.out.print("Nhap canh: ");
         int Canh = scanner.nextInt();
         
-        int TheTich = Canh * Canh * Canh;
-        
+        //Tính thể thích và in kết quả
+        int TheTich = Canh * Canh * Canh;      
         System.out.print("The tich cua hinh la: " + TheTich);
     }
 }
